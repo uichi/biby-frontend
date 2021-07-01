0a91dea9e0258ba50c7625775fc23cdfcb3827db
uichi <curry_and_coffee@ykonishi.tokyo>
2021-07-01 18:19:00 +0900

    Develop care log process.

--- biby/src/api/CareLog.ts
+++ biby/src/api/CareLog.ts
@@ -1,21 +1,40 @@
 import { careLogUrl } from "./Config";
 import { Pet } from "../types";
 
+export const getCareLog = (id: string, token: string): Promise<any> | null => {
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  };
+  return fetch(careLogUrl + `${id}/`, options)
+    .then((res) => {
+      if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .then((json) => json)
+    .catch(() => null);
+};
+
 export const postCareLog = (
-  care_category: string,
+  care_category_pk: string,
   date_time: string | null,
   text: string | null,
   integer: number | null,
   float: number | null,
   memo: string | null,
+  user_pk: string | null,
   token: string
 ): Promise<any> | null => {
   const body = JSON.stringify({
-    care_category,
+    care_category_pk,
     date_time,
     text,
     integer,
     memo,
+    user_pk,
     float,
   });
   const options = {
@@ -28,7 +47,87 @@ export const postCareLog = (
   };
   return fetch(careLogUrl, options)
     .then((res) => {
+      if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .then((json) => json)
+    .catch(() => null);
+};
+
+export const getCareLogs = (
+  meId: string,
+  token: string
+): Promise<any[]> | [] => {
+  const query_params = new URLSearchParams({
+    user: meId,
+  });
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  };
+  return fetch(careLogUrl + `?${query_params}`, options)
+    .then((res) => {
+      if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .then((json) => json.results)
+    .catch(() => []);
+};
+
+export const patchCareLog = (
+  id: string,
+  care_category_pk: string,
+  date_time: string | null,
+  text: string | null,
+  integer: number | null,
+  float: number | null,
+  memo: string | null,
+  user_pk: string | null,
+  token: string
+): Promise<any> | null => {
+  const body = JSON.stringify({
+    care_category_pk,
+    date_time,
+    text,
+    integer,
+    memo,
+    user_pk,
+    float,
+  });
+  const options = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+    body,
+  };
+  return fetch(careLogUrl + `${id}/`, options)
+    .then((res) => {
       //       if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .then((json) => json)
+    .catch(() => null);
+};
+
+export const deleteCareLog = (
+  id: string,
+  token: string
+): Promise<any> | null => {
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  };
+  return fetch(careLogUrl + `${id}/`, options)
+    .then((res) => {
+      if (!res.ok) throw new Error();
       return res.json();
     })
     .then((json) => json)
--- biby/src/components/CareLogEdit.tsx
+++ biby/src/components/CareLogEdit.tsx
@@ -9,17 +9,115 @@ import {
   AlertDialog,
   TextArea,
   NumberField,
-  Checkbox,
+  Picker,
+  Item,
 } from "@adobe/react-spectrum";
-import { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { useCookies } from "react-cookie";
+import { useHistory } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { getCategories } from "../api/CareCategory";
+import { getCareLog, patchCareLog, deleteCareLog } from "../api/CareLog";
+import { CareCategory } from "../types";
+import {
+  notifySuccessSave,
+  notifyErrorSave,
+  notifyEssentialValueIsEmpty,
+} from "./common/toast";
 
 const CareLogEdit = (): JSX.Element => {
-  const [selected, setSelected] = useState<boolean>(false);
-
+  const [cookies, setCookie] = useCookies(); // eslint-disable-line
+  const [inputType, setInputType] = useState<any>();
+  const [inputTypes, setInputTypes] = useState<any[]>([]);
+  const [categories, setCategories] = useState<CareCategory[]>([]);
+  const [dateTime, setDateTime] = useState<string>("");
+  const [text, setText] = useState<string | null>(null);
+  const [integer, setInteger] = useState<number | null>(null);
+  const [float, setFloat] = useState<number | null>(null);
+  const [memo, setMemo] = useState<string | null>(null);
+  const [selectedCareLogId, setSelectedCareLogId] = useState<string>("");
+  const history = useHistory();
+  const [fieldTypeId, setFieldTypeId]: [
+    string,
+    Dispatch<SetStateAction<any>> // HACK: 型定義見直す
+  ] = useState<string>("");
+  if (!cookies.authToken) history.push("/login");
+  useEffect(() => {
+    (async () => {
+      const selectedCareLogId = location.pathname.split("/").slice(-1)[0];
+      setSelectedCareLogId(selectedCareLogId);
+      const resultGetLog = await getCareLog(
+        selectedCareLogId,
+        cookies.authToken
+      );
+      setText(resultGetLog.text);
+      setInteger(resultGetLog.integer);
+      setFloat(resultGetLog.float);
+      setFieldTypeId(resultGetLog.care_category.id);
+      const today = new Date(resultGetLog.date_time);
+      const year = today.getFullYear();
+      const month = ("00" + (today.getMonth() + 1)).slice(-2);
+      const day = ("00" + today.getDate()).slice(-2);
+      const hour = today.getHours();
+      const minute = today.getMinutes();
+      setDateTime(`${year}-${month}-${day}T${hour}:${minute}`);
+      setMemo(resultGetLog.memo);
+      setInputType({
+        id: resultGetLog.care_category.id,
+        name: resultGetLog.care_category.input_type,
+        unit: resultGetLog.care_category.unit,
+      });
+      const resultGetCareCategories = await getCategories(
+        cookies.meId,
+        cookies.authToken
+      );
+      setCategories(resultGetCareCategories);
+      setInputTypes(
+        resultGetCareCategories.map((value) => ({
+          id: value.id,
+          name: value.input_type,
+          unit: value.unit,
+        }))
+      );
+    })();
+  }, []);
+  const updateCareLog = async () => {
+    if (fieldTypeId === "" || dateTime === "") {
+      notifyEssentialValueIsEmpty();
+      return;
+    }
+    const resultUpdateCareLog = await patchCareLog(
+      selectedCareLogId,
+      fieldTypeId,
+      dateTime,
+      inputType.name === "text" ? text : null,
+      inputType.name === "integer" ? integer : null,
+      inputType.name === "float" ? float : null,
+      memo,
+      cookies.meId,
+      cookies.authToken
+    );
+    console.log(resultUpdateCareLog);
+    if (!resultUpdateCareLog) {
+      notifyErrorSave();
+      return;
+    }
+    notifySuccessSave();
+  };
+  // HACK: 型指定見直す
+  const onChangeInputType = (categoryId: any): void => {
+    setInputType(inputTypes.find((value) => value.id === categoryId));
+    setFieldTypeId(categoryId);
+  };
+  const removeCareLog = async () => {
+    await deleteCareLog(selectedCareLogId, cookies.authToken);
+    history.push("/care/logs");
+  };
   return (
     <Provider theme={defaultTheme} colorScheme="dark">
+      <Toaster position="top-center" />
       <Header />
       <View
         backgroundColor="gray-200"
@@ -32,35 +130,60 @@ const CareLogEdit = (): JSX.Element => {
           <h3 id="label-3">記録</h3>
           <Form aria-labelledby="label-3" necessityIndicator="icon">
             <TextField
-              label="テキスト"
-              placeholder="テキスト"
+              type="datetime-local"
+              label="日付"
+              value={dateTime}
+              onChange={setDateTime}
               isRequired={true}
             />
-            <NumberField
-              label="整数"
-              defaultValue={4}
-              minValue={0}
-              formatOptions={{
-                style: "unit",
-                unit: "minute",
-                unitDisplay: "long",
-              }}
+            <Picker
+              label="カテゴリを選択してください"
+              items={categories.map((value) => ({
+                id: value.id,
+                name: value.name,
+                input_type: value.input_type,
+              }))}
+              selectedKey={fieldTypeId}
+              onSelectionChange={onChangeInputType}
+              isRequired={true}
+            >
+              {(item) => <Item>{item.name}</Item>}
+            </Picker>
+            {((): any => {
+              if (!inputType) return;
+              if (inputType.name === "text")
+                return (
+                  <TextField
+                    label="テキスト"
+                    placeholder=""
+                    defaultValue={!text ? "" : text}
+                    onChange={setText}
+                  />
+                );
+              if (inputType.name === "integer")
+                return (
+                  <NumberField
+                    label={"整数" + " (" + inputType.unit + ")"}
+                    defaultValue={!integer ? 0 : integer}
+                    onChange={setInteger}
+                  />
+                );
+              if (inputType.name === "float")
+                return (
+                  <NumberField
+                    label={"小数" + " (" + inputType.unit + ")"}
+                    defaultValue={!float ? 0 : float}
+                    onChange={setFloat}
+                  />
+                );
+            })()}
+            <TextArea
+              label="メモ"
+              height="size-1600"
+              defaultValue={!memo ? "" : memo}
+              onChange={setMemo}
             />
-            <NumberField
-              label="小数"
-              defaultValue={4}
-              minValue={0}
-              formatOptions={{
-                style: "unit",
-                unit: "inch",
-                unitDisplay: "long",
-              }}
-            />
-            <Checkbox isSelected={selected} onChange={setSelected}>
-              Subscribe (controlled)
-            </Checkbox>
-            <TextArea label="メモ" />
-            <ActionButton type="submit" staticColor="white">
+            <ActionButton staticColor="white" onPress={updateCareLog}>
               保存
             </ActionButton>
             <DialogTrigger>
@@ -69,9 +192,10 @@ const CareLogEdit = (): JSX.Element => {
                 variant="destructive"
                 title="削除しますか？"
                 primaryActionLabel="削除"
+                onPrimaryAction={removeCareLog}
                 cancelLabel="キャンセル"
               >
-                これまで記録したデータも削除されます。
+                一度削除すると復元はできません。
               </AlertDialog>
             </DialogTrigger>
           </Form>
