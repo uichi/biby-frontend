5f4490b3655dc4a94c97b44e745b7c96e4784819
uichi <37263474+uichi@users.noreply.github.com>
2021-09-25 21:33:39 +0900

    Add login button header.

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -4,14 +4,17 @@ import { Link as RouterLink } from "react-router-dom";
 const Header = (): JSX.Element => {
   scrollToTop();
   return (
-    <div className="bg-white fixed w-full z-10 top-0 shadow">
-      <div className="w-full py-3">
+    <div className="w-full flex bg-white fixed w-full z-10 top-0 shadow">
+      <div className="w-1/2 py-3">
         <RouterLink
           className="text-gray-900 text-xl no-underline hover:no-underline font-bold px-3"
           to="/"
         >
           biby
         </RouterLink>
+      </div>
+      <div className="w-1/2 p-3 pull-right">
+        <button className="bg-green-500 hover:bg-green-700 text-white hover:text-white py-1 px-2 rounded float-right">ログイン</button>
       </div>
     </div>
   );
